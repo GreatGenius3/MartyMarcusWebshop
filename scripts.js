830a3fb2f87190a1cd849633cdfa7d0db66bfe75
Martin Danmo <martindanmo81@gmail.com>
2026-03-23 17:49:00 +0100

    Lite ändringar

--- scripts.js
+++ scripts.js
@@ -1,17 +1,22 @@
 // Funktion f?r att h?mta produkter fr?n Fake Store API
 // Anv?nd async/await f?r att hantera asynkrona operationer
-async function fetchProducts() {
-    try {
+async function fetchProducts()
+{
+    try 
+    {
         const response = await fetch('https://fakestoreapi.com/products');
         const products = await response.json();
         displayProducts(products);
-    } catch (error) {
+    } 
+    catch (error)
+    {
         console.error('Fel vid h?mtning av produkter:', error);
     }
 }
 
 // Funktion f?r att visa produkter p? sidan
-function displayProducts(products) {
+function displayProducts(products)
+{
     const productList = document.querySelector('.product-list');
     productList.innerHTML = ''; // T?m den befintliga listan
 
